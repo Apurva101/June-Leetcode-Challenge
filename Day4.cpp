06f9213fbcb0822bff917fbfb43a4fe54cceceb7
Apurva101 <46521835+Apurva101@users.noreply.github.com>
2020-06-13 15:09:41 +0530

    Update Day4.cpp

--- Day4.cpp
+++ Day4.cpp
@@ -1,36 +1,33 @@
 /*
 
-Given an array w of positive integers, where w[i] describes the weight of index i, write a function pickIndex which randomly picks an index in proportion to its weight.
+Write a function that reverses a string. The input string is given as an array of characters char[].
 
-For example, given an input list of values [1, 9], when we pick up a number out of it, the chance is that 9 times out of 10 we should pick the number 9 as the answer.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters.
+
+ 
 
 Example 1:
 
-Input: 
-["Solution","pickIndex"]
-[[[1]],[]]
-Output: [null,0]
-Example 2:
-
-Input: 
-["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
-[[[1,3]],[],[],[],[],[]]
-Output: [null,0,1,1,1,0]
-
+Input: ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
 */
 class Solution {
 public:
-    vector<int> prob;
-    Solution(vector<int>& w) {
-        
-         prob.push_back(w[0]);
-        for(int i=1;i<w.size();++i)
-            prob.push_back(prob[i-1]+w[i]);
+    void swap(char *a, char *b)
+    {
+        char t;
+ 
+   t  = *b;
+   *b = *a;
+   *a = t;
     }
-    
-    int pickIndex() {
+    void reverseString(vector<char>& s) {
+        int n=s.size();
         
-        int num=rand()%prob.back()+1;
-        return lower_bound(prob.begin(), prob.end(), num)-prob.begin();
+        for(int i=0;i<n/2;++i)
+            swap(&s[i],&s[n-1-i]);
+            
     }
 };
